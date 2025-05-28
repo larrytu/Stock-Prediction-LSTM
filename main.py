5fc0aea1832c277733f911f8ac8d30c8faacc4aa
Larry Tu <larrylamtu@gmail.com>
2025-05-27 21:38:31 -0700

    removing files

--- main.py
+++
@@ -1,6 +0,0 @@
-from data_preprossessing import check_missing_values_in_csv
-
-
-data_folder = "./Data"
-dataframes = check_missing_values_in_csv(data_folder)
-print("\nDataFrames stored for each file:")
